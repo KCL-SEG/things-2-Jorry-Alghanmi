b6f69e2aede540602492319abb5a50bd5c6add9a
Jorry-Alghanmi <127518285+Jorry-Alghanmi@users.noreply.github.com>
2023-12-01 10:01:23 +0000

    implement the exercises

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,18 @@
 """Forms of the project."""
 
 # Create your forms here.
+from django import forms
+from django.core.validators import MinValueValidator, MaxValueValidator
+from .models import Thing
+
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+
+    name = forms.CharField(max_length=35, widget=forms.TextInput(attrs={'max_length': '35'}), required=True)
+    description = forms.CharField(max_length=120, widget=forms.Textarea, required=False)
+    quantity = forms.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(50)],
+        widget=forms.NumberInput(attrs={'min': '0', 'max': '50'})
+    )
--- things/views.py
+++ things/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
 
+from django.shortcuts import render, redirect
+from .forms import ThingForm  # Import your ThingForm
+
 def home(request):
+    if request.method == 'POST':
+        form = ThingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = ThingForm()
     return render(request, 'home.html', {'form': form})
+
